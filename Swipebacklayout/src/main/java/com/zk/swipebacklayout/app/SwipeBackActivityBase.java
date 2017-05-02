a6cb64bff74ece71dbc719498dfd03fe0b37f67c
zkzqzzz <zkzqzzz@gmail.com>
2017-05-02 12:23:40 +0800

    Initial commit

---
+++ Swipebacklayout/src/main/java/com/zk/swipebacklayout/app/SwipeBackActivityBase.java
@@ -0,0 +1,21 @@
+package com.zk.swipebacklayout.app;
+
+
+import com.zk.swipebacklayout.SwipeBackLayout;
+
+/**
+ */
+public interface SwipeBackActivityBase {
+    /**
+     * @return the SwipeBackLayout associated with this activity.
+     */
+    SwipeBackLayout getSwipeBackLayout();
+
+    void setSwipeBackEnable(boolean enable);
+
+    /**
+     * Scroll out contentView and finish the activity
+     */
+    void scrollToFinishActivity();
+
+}
